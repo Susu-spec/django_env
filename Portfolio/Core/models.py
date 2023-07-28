686adb0061a81ec3fe10a00408fc0fd3cd775550
Susu-spec <suleimansuwaibat@gmail.com>
2023-07-28 06:13:01 -0700

    Edit: models.py

--- Portfolio/Core/models.py
+++ Portfolio/Core/models.py
@@ -4,4 +4,7 @@ class Feedback(models.Model):
     name = models.charField(max_length=100)
     email = models.EmailField(max_length)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} = {self.email}"
     
